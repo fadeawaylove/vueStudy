dbc5ebfade1f8b501dba0fb82caed427eb6f0548
邓润庭 <1032939141@qq.com>
2020-02-14 15:23:40 +0800

    解决xadmin保存文章时不能自动加上作者

--- blog/adminx.py
+++ blog/adminx.py
@@ -4,11 +4,15 @@ import xadmin
 from .models import Post, Category, Tag
 
 
+@xadmin.sites.register(Post)
 class PostAdmin(object):
     list_display = ['title', 'create_time', 'update_time', 'category', 'author']
     fields = ['title', 'body', 'excerpt', 'category', 'tags']
     list_filter = ["title", "category", "tags"]
 
+    def save_models(self):
+        self.new_obj.author = self.request.user
+        super().save_models()
 
 class CategoryAdmin(object):
     fields = ['name']
@@ -18,6 +22,6 @@ class TagAdmin(object):
     fields = ['name']
     list_filter = ["name"]
 
-xadmin.site.register(Post, PostAdmin)
+# xadmin.site.register(Post, PostAdmin)
 xadmin.site.register(Category, CategoryAdmin)
 xadmin.site.register(Tag, TagAdmin)
