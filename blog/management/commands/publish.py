40744b8b2ebcf6f28eda64de95ff5166414bf304
邓润庭 <1032939141@qq.com>
2020-05-17 16:10:56 +0800

    add

--- blog/management/commands/publish.py
+++ blog/management/commands/publish.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
                 os.system("docker-compose -f local.yml up --build")
             if env == "pro":
                 # os.system("fab -H daigua@149.129.67.128 --prompt-for-login-password -p deploy")
-                os.system("fab -H daigua@141.164.51.108 --prompt-for-login-password -p deploy")
+                os.system("fab -H daigua@141.164.55.101 --prompt-for-login-password -p deploy")
 
         except Exception as e:
             self.stdout.write(self.style.ERROR(e))
