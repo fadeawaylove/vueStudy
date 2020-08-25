b613ca7404abeb15c18b4a20f07c3be95597b922
邓润庭 <dengrt@akulaku.com>
2020-08-25 13:20:05 +0800

    fix

--- dog_blog/settings/local.py
+++
@@ -1,6 +0,0 @@
-from .common import *
-
-SECRET_KEY = 'development-secret-key'
-DEBUG = True
-ALLOWED_HOSTS = ['*']
-FILE_HOST = "127.0.0.1:8000"
--- dog_blog/settings/product.py
+++
@@ -1,8 +0,0 @@
-from .common import *
-
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-DEBUG = False
-ALLOWED_HOSTS = ["127.0.0.1", 'localhost','.fadeaway.ltd']
-FILE_HOST = "dog.fadeaway.ltd"
-
-
